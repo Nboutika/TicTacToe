41f52ca530df26d73326db6bc92a48759b3fc101
Manissar <boutikarnassim@gmail.com>
2021-03-04 21:13:19 +0100

    New font added

--- src/Window.java
+++ src/Window.java
@@ -1,32 +1,33 @@
 import javax.swing.*;
-import javax.swing.plaf.basic.BasicLookAndFeel;
-import javax.swing.plaf.metal.MetalLookAndFeel;
+
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 public class Window extends JFrame {
 
     JButton[] buttons = new JButton[9];
     JPanel mainPanel = new JPanel();
-    JLabel test;
+    JLabel mainText;
+    protected Font duran;
 
     public Window(String title) throws HeadlessException {
 
+
+        // Setting up the window
         super(title);
+
         setSize(450,400);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
         setResizable(false);
         mainPanel.setLayout(new GridLayout(3,3));
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        /*try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
 
-            }
-           */
+
+        // Setting Nimbus LookAndFeel
         try {
             UIManager.setLookAndFeel(new NimbusLookAndFeel());
         } catch (UnsupportedLookAndFeelException unsupportedLookAndFeelException) {
@@ -34,23 +35,48 @@ public class Window extends JFrame {
            }
 
 
+
+
+
+
+        // creating buttons
         for (int i = 0; i < 9; i++) {
                 buttons[i] = new JButton("");
-                buttons[i].setFocusable(false);
-                buttons[i].addActionListener(this::actionPerformed);
-                mainPanel.add(buttons[i]);
+                buttons[i].setFocusable(false); // making sure they can't be focused (like with tab)
+                buttons[i].addActionListener(this::actionPerformed); // adding an action listener for when we press it
+                mainPanel.add(buttons[i]); // adding buttons to our mainPanel 3 by 3 grid
 
 
         }
-        add(mainPanel,BorderLayout.CENTER);
+        add(mainPanel,BorderLayout.CENTER); // adding the mainPanel to the center of our window
 
-        test = new JLabel("Ceci est un message test", SwingConstants.CENTER);
-        add(test, BorderLayout.NORTH);
+        mainText = new JLabel("", SwingConstants.CENTER); // empty label for information
+        mainText.setFont(new Font("duran", Font.PLAIN, 16));
+        mainText.setPreferredSize(new Dimension(0,25));
+        mainText.setForeground(new Color(0, 158, 143));
+        add(mainText, BorderLayout.NORTH);
 
         setVisible(true);
+        JOptionPane.showMessageDialog(this,"press R to restart");
+
+
 
     }
 
+
+    protected void font(){
+        // creating new font
+
+        try {
+            duran = Font.createFont(Font.TRUETYPE_FONT, new File("FontsFree-Net-Duran-Light.ttf"));
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("FontsFree-Net-Duran-Light.ttf")));
+
+        }catch (IOException | FontFormatException e){
+            e.printStackTrace();
+
+        }
+    }
     protected void actionPerformed(ActionEvent e){
 
     }
