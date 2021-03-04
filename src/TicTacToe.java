bc9486be10121cc4e268bb134e30f8848612252e
Manissar <boutikarnassim@gmail.com>
2021-03-04 21:14:54 +0100

    Working game added : KeyAdapter, win method, and actionlistener revisited

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,22 +1,120 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.security.Key;
 
-public class TicTacToe extends Window {
+public class TicTacToe extends Window  {
+
+    private boolean state = true;
+    private Player turn = Player.PLAYERX;
+
 
 
     public TicTacToe(String title) throws HeadlessException {
         super(title);
-        System.out.println(buttons[0].getText());
+        mainText.setText(turn.getAbbreviation() + " Turns");
+        this.setFocusable(true);
+        this.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                keyHandler(e);
+            }
+        });
 
     }
 
+
+
+
+    public int[] win(){
+        if(buttons[0].getText() == buttons[1].getText() && buttons[2].getText() == buttons[0].getText()
+                && buttons[0].getText() != "" )
+            return new int[]{1,0,1,2};
+        else if(buttons[3].getText() == buttons[4].getText() && buttons[5].getText() == buttons[3].getText() &&
+                buttons[3].getText() != "")
+            return new int[]{1,3,4,5};
+        else if(buttons[6].getText() == buttons[7].getText() && buttons[8].getText() == buttons[6].getText() &&
+                buttons[6].getText() != "")
+            return new int[]{1,6,7,8};
+        else if(buttons[0].getText() == buttons[3].getText() && buttons[6].getText() == buttons[0].getText() &&
+                buttons[0].getText() != "")
+            return new int[]{1,0,3,6};
+        else if(buttons[1].getText() == buttons[4].getText() && buttons[7].getText() == buttons[1].getText() &&
+                buttons[1].getText() != "")
+            return new int[]{1,1,4,7};
+        else if(buttons[2].getText() == buttons[5].getText() && buttons[8].getText() == buttons[2].getText() &&
+                buttons[2].getText() != "")
+            return new int[]{1,2,5,8};
+        else if(buttons[0].getText() == buttons[4].getText() && buttons[8].getText() == buttons[0].getText() &&
+                buttons[0].getText() != "")
+            return new int[]{1,0,4,8};
+        else if(buttons[2].getText() == buttons[4].getText() && buttons[6].getText() == buttons[2].getText() &&
+                buttons[2].getText() != "")
+            return new int[]{1,2,4,6};
+        return new int[] {0};
+    }
     @Override
     protected void actionPerformed(ActionEvent e) {
         JButton button = (JButton) e.getSource();
+        if (state) {
+            if (turn == Player.PLAYERX && button.getText() == "") {
+                button.setForeground(new Color(17, 19, 92, 255));
+                button.setText("X");
+                button.setFont(new Font("duran", Font.PLAIN, 36));
+                if (win()[0] == 1) {
+                    int[] pos = new int[] {win()[1], win()[2], win()[3]};
+                    mainText.setText("X won");
+                    mainText.setFont(new Font("duran", Font.PLAIN, 16));
 
-        button.setForeground(new Color(69,73,85));
-        button.setBackground(new Color(114,176,29, 179));
-        button.setText("Test");
+                    state = false;
+                    for (int i = 0; i < pos.length; i++) {
+                        buttons[pos[i]].setBackground(new Color(18, 222, 0, 171));
+                    }
+
+                } else{
+                    turn = Player.PLAYERO;
+                    mainText.setText(turn.getAbbreviation() + " Turns");
+                }
+            } else if (turn == Player.PLAYERO && button.getText() == "") {
+                button.setForeground(new Color(17, 19, 92, 255));
+                button.setText("O");
+                button.setFont(new Font("duran", Font.PLAIN, 36));
+                if (win()[0] == 1) {
+                    int[] pos = new int[] {win()[1], win()[2], win()[3]};
+                    mainText.setText("O won");
+                    mainText.setFont(new Font("duran", Font.PLAIN, 16));
+
+                    state = false;
+                    for (int i = 0; i < pos.length; i++) {
+                        buttons[pos[i]].setBackground(new Color(18, 222, 0, 171));
+                    }
+
+                } else {
+                    turn = Player.PLAYERX;
+                    mainText.setText(turn.getAbbreviation() + " Turns");
+                }
+            }
+        }
     }
+
+    private void keyHandler(KeyEvent e){
+            int key = e.getKeyCode();
+
+            if(key == KeyEvent.VK_R){
+                for (int i = 0; i < 9; i++) {
+                    buttons[i].setText("");
+                    buttons[i].setBackground(null);
+                }
+                state = true;
+                turn = Player.PLAYERX;
+                mainText.setText(turn.getAbbreviation() + " Turns");
+            }
+    }
+
+
+
 }
+
+
